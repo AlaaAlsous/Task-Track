57b91cadbb653759b05fcf0909fb32b12b4eeda5
Alaa Alsous <lawyeralaa@gmail.com>
2025-12-27 13:52:27 +0100

    Add static file serving and GET /api/tasks API

--- server.js
+++ server.js
@@ -4,8 +4,14 @@ import express from "express";
 const app = express();
 const port = 80;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(express.static("public"));
+
+app.get("/index.html", (req, res) => {
+  res.redirect("/");
+});
+
+app.get("/api/tasks", (req, res) => {
+  res.json(tasks);
 });
 
 app.listen(port, () => {
